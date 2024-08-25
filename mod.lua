fdbcd05aaff6c9cd0308a4533754c6d87717cc30
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-08-25 15:03:56 +0200

    Update 1.1
    Umbau aus Basis Mod
    - Sound
    -Zg2 Tafeln getauscht
    - Fake hinzugefügt

--- mod.lua
+++ mod.lua
@@ -1,7 +1,7 @@
 ﻿function data()
 return {
 	info = {
-		minorVersion = 0,
+		minorVersion = 1,
 		severityAdd = "NONE",
 		severityRemove = "WARNING",
 		name = _("mod_name"),
@@ -12,15 +12,57 @@ return {
 		        role = "CREATOR",
 		    },
 		},
-		tags = { "europe", "germany", "triebwagen", "railcar", "austria", "GKB", "AKN", "HLB" },
+		tags = { "europe", "deutschland", "germany", "db", "waggon", "Ballast", },
+		minGameVersion = 0,
 		dependencies = { },
 		url = { "" },
+
+        params = {
+			{
+				key = "schotterfake",
+				name = _("Fake_schotter_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_schotter_desc"),
+				defaultIndex = 0,
+			},			
+			{
+				key = "soundCheck",
+				name = _("sound_check"),
+				uiType = "CHECKBOX",
+				values = { "No", "Yes", },	
+				tooltip = _("option_sound_check"),			
+				defaultIndex = 1,	
+			},
+        },
 	},
 	options = {
 	},
+	
+	runFn = function (settings, modParams)
+	local params = modParams[getCurrentModId()]
 
-	runFn = function (settings)
+        local hidden = {
+			["1_fake.mdl"] = true, 
+			["2_fake.mdl"] = true, 
+			["3_fake.mdl"] = true, 
+			["4_fake.mdl"] = true, 
+        }
 
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/Schotterwagen_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["schotterfake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+
+		addModifier( "loadModel", metadataHandler )
 	end
 	}
 end
--- strings.lua
+++ strings.lua
@@ -9,10 +9,25 @@ return {
 
         ["Schotterwagen_1_name"] = "Ballast wagon DB (1948)",
         ["Schotterwagen_2_name"] = "Ballast wagon DB AG (1994)",
-        ["Schotterwagen_3_name"] = "Ballast wagon DRG (1934)",
+        ["Schotterwagen_3_name"] = "Ballast wagon DRG (1938)",
         ["Schotterwagen_4_name"] = "Ballast wagon DR (1948)",
 		["Schotterwagen_desc"] = "Ballast wagons are not freight wagons in the conventional sense, but rail service vehicles. In contrast to normal bulk goods wagons, they have two centrally arranged chutes in addition to the side chutes, so that the area between the tracks can also be ballasted. In addition to the actual function of transporting ballast, the purpose of the vehicle is also the targeted placement of the load.",
 
+        ["Fake_schotter_wagen"] = "Activate ballast wagon fake versions?",
+        ["option_fake_schotter_desc"] = "Should the fake versions of the gravel wagons be available in the game?",
+		
+		["sound_check"] = "Activate custom sound?",
+		["option_sound_check"] = "Should the extra sound be activated?",
+		
+		--- Fake ---
+		
+        ["Schotterwagen_menu_fake"] = "Ballast wagons Fake",
+		
+        ["Schotterwagen_1_name_fake"] = "Ballast wagon DB (1948) Fake",
+        ["Schotterwagen_2_name_fake"] = "Ballast wagon DB AG (1994) Fake",
+        ["Schotterwagen_3_name_fake"] = "Ballast wagon DRG (1938) Fake",
+        ["Schotterwagen_4_name_fake"] = "Ballast wagon DR (1948) Fake",
+	
 	},
 	de = {
 
@@ -23,10 +38,25 @@ return {
 
         ["Schotterwagen_1_name"] = "Schotterwagen DB (1948)",
         ["Schotterwagen_2_name"] = "Schotterwagen DB AG (1994)",
-        ["Schotterwagen_3_name"] = "Schotterwagen DRG (1934)",
+        ["Schotterwagen_3_name"] = "Schotterwagen DRG (1938)",
         ["Schotterwagen_4_name"] = "Schotterwagen DR (1948)",
 		["Schotterwagen_desc"] = "Schotterwagen zählen nicht zu den Güterwagen im herkömmlichen Sinne, sondern zu den Bahndienstfahrzeugen. Im Gegensatz zu normalen Schüttgutwagen verfügen sie neben den seitlichen Schüttvorrichtungen über zwei mittig angeordnete Schütten, um auch den Bereich zwischen den Gleisen beschottern zu können. Neben der eigentlichen Funktion des Schottertransports ist als auch die gezielte Einbringung des Ladegutes Zweck des Fahrzeuges.",
 
+        ["Fake_schotter_wagen"] = "Schotterwagen Fake-Versionen aktivieren?",
+        ["option_fake_schotter_desc"] = "Sollen die Fake-Versionen der Schotterwagen verfügbar sein im Spiel?",
+
+		["sound_check"] = "Eigenes Soundset aktivieren?",
+		["option_sound_check"] = "Soll der extra Sound aktiviert sein?",
+		
+		--- Fake ---
+		
+        ["Schotterwagen_menu_fake"] = "Schotterwagen Fake",
+
+        ["Schotterwagen_1_name_fake"] = "Schotterwagen DB (1948) Fake",
+        ["Schotterwagen_2_name_fake"] = "Schotterwagen DB AG (1994) Fake",
+        ["Schotterwagen_3_name_fake"] = "Schotterwagen DRG (1938) Fake",
+        ["Schotterwagen_4_name_fake"] = "Schotterwagen DR (1948) Fake",
+		
 	},
 }
 end
