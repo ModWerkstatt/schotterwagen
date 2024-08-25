fdbf5bd484cbaa72929ec98f446d3ffaad93ce9f
Florian Thiel <flth@flth.de>
2024-08-25 14:09:49 +0200

    Initial

---
+++ strings.lua
@@ -0,0 +1,32 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Ballast wagons",
+		["mod_desc"] = "Ballast wagons are not freight wagons in the conventional sense, but rail service vehicles. In contrast to normal bulk goods wagons, they have two centrally arranged chutes in addition to the side chutes, so that the area between the tracks can also be ballasted. In addition to the actual function of transporting ballast, the purpose of the vehicle is also the targeted placement of the load. Two operators are needed to evenly dose the ballast via the four control levers for opening and closing the troughs. During the emptying process, the wagon is slowly pulled over the track to be ballasted. The ballast is poured well above the top edge of the sleepers, as the ballast is tamped under the sleepers during the subsequent tamping process and the entire track is slightly tamped up in many places.",
+        
+        ["Schotterwagen_menu"] = "Ballast wagons",
+
+        ["Schotterwagen_1_name"] = "Ballast wagon DB (1948)",
+        ["Schotterwagen_2_name"] = "Ballast wagon DB AG (1994)",
+        ["Schotterwagen_3_name"] = "Ballast wagon DRG (1934)",
+        ["Schotterwagen_4_name"] = "Ballast wagon DR (1948)",
+		["Schotterwagen_desc"] = "Ballast wagons are not freight wagons in the conventional sense, but rail service vehicles. In contrast to normal bulk goods wagons, they have two centrally arranged chutes in addition to the side chutes, so that the area between the tracks can also be ballasted. In addition to the actual function of transporting ballast, the purpose of the vehicle is also the targeted placement of the load.",
+
+	},
+	de = {
+
+		["mod_name"] = "Schotterwagen",
+		["mod_desc"] = "Schotterwagen zählen nicht zu den Güterwagen im herkömmlichen Sinne, sondern zu den Bahndienstfahrzeugen. Im Gegensatz zu normalen Schüttgutwagen verfügen sie neben den seitlichen Schüttvorrichtungen über zwei mittig angeordnete Schütten, um auch den Bereich zwischen den Gleisen beschottern zu können. Neben der eigentlichen Funktion des Schottertransports ist als auch die gezielte Einbringung des Ladegutes Zweck des Fahrzeuges. Zwei Bediener sind notwendig, um über die vier Bedienungshebel zum Öffnen und Schließen der Schütten den Schotter gleichmäßig zu dosieren. Während des Entleerungsvorganges wird der Wagen langsam über die zu beschotternde Strecke gezogen. Dabei wird deutlich über Schwellenoberkante geschüttet, da der Schotter beim anschließenden Stopfvorgang unter die Schwellen gestopft und das gesamte Gleis an vielen Stellen leicht hochgestopft wird.",
+        
+        ["Schotterwagen_menu"] = "Schotterwagen",
+
+        ["Schotterwagen_1_name"] = "Schotterwagen DB (1948)",
+        ["Schotterwagen_2_name"] = "Schotterwagen DB AG (1994)",
+        ["Schotterwagen_3_name"] = "Schotterwagen DRG (1934)",
+        ["Schotterwagen_4_name"] = "Schotterwagen DR (1948)",
+		["Schotterwagen_desc"] = "Schotterwagen zählen nicht zu den Güterwagen im herkömmlichen Sinne, sondern zu den Bahndienstfahrzeugen. Im Gegensatz zu normalen Schüttgutwagen verfügen sie neben den seitlichen Schüttvorrichtungen über zwei mittig angeordnete Schütten, um auch den Bereich zwischen den Gleisen beschottern zu können. Neben der eigentlichen Funktion des Schottertransports ist als auch die gezielte Einbringung des Ladegutes Zweck des Fahrzeuges.",
+
+	},
+}
+end
